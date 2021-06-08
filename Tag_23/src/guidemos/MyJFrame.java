137e26c165d8be46de62f641cf92a8affb17982a
Daniel Schirmer <d.schirmer1987@gmail.com>
2021-06-08 08:00:18 +0200

    c

--- Tag_23/src/guidemos/MyJFrame.java
+++ Tag_23/src/guidemos/MyJFrame.java
@@ -4,13 +4,11 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class MyJFrame extends JFrame{
-	JLabel label = new JLabel("Ein Label");
 
 	public MyJFrame() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(640, 480);
 		this.setTitle("Demo JFrame");
 		this.setLocationRelativeTo(null);
-		this.add(label);
 	}
 }

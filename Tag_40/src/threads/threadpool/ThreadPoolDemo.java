1f6328f722e3a8516b13c60aa75f4bf70c742146
Daniel Schirmer <d.schirmer1987@gmail.com>
2021-07-08 11:02:21 +0200

    ThreadPool Demo

--- Tag_40/src/threads/threadpool/ThreadPoolDemo.java
+++ Tag_40/src/threads/threadpool/ThreadPoolDemo.java
@@ -1,8 +1,11 @@
 package threads.threadpool;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ThreadPoolDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Runnable r1 = () -> {
 			System.out.println("r1.1 " + Thread.currentThread().getName());
 			System.out.println("r1.2 " + Thread.currentThread().getName());
@@ -12,6 +15,19 @@ public class ThreadPoolDemo {
 			System.out.println("r2.1 " + Thread.currentThread().getName());
 			System.out.println("r2.2 " + Thread.currentThread().getName());
 		};
+		
+		ExecutorService executor = Executors.newCachedThreadPool();
+		ExecutorService executor2 = Executors.newCachedThreadPool();
+		
+		executor.execute(r1);
+		executor.execute(r2);
+		
+		Thread.sleep(5000);
+		
+		executor2.execute(r1);
+		executor2.execute(r2);
+		
+		executor.shutdown();
 
 	}
 

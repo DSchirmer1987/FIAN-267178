f74f452b3d6906095bf76b76cd0268484d25f084
Daniel Schirmer <d.schirmer1987@gmail.com>
2021-07-22 12:52:04 +0200

    EASTEREGG FIX

--- Tag_50_Part2/src/main/java/com/fian267180x/Tag_50_Part2/Easteregg.java
+++ Tag_50_Part2/src/main/java/com/fian267180x/Tag_50_Part2/Easteregg.java
@@ -1,5 +1,5 @@
 package com.fian267180x.Tag_50_Part2;
 
 public interface Easteregg {
-	public final String EASTEREGG = "https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1";
+	public final String EASTEREGG = "https://www.youtube.com/embed/DLzxrzFCyOs?autoplay=1";
 }

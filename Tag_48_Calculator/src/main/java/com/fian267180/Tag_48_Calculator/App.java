3bdf0dcf7523b2e2dcee96d11770d0421943a49d
Daniel Schirmer <d.schirmer1987@gmail.com>
2021-07-22 08:57:08 +0200

    Font

--- Tag_48_Calculator/src/main/java/com/fian267180/Tag_48_Calculator/App.java
+++ Tag_48_Calculator/src/main/java/com/fian267180/Tag_48_Calculator/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,7 +18,8 @@ public class App extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Calculator.fxml"));
         Scene scene = new Scene(root);
-//        scene.getStylesheets().add(getClass().getResource("calculator.css").toExternalForm());
+        Font.loadFont(getClass().getResourceAsStream("digital_7_new.ttf"), 60);
+        scene.getStylesheets().add(getClass().getResource("calculator.css").toExternalForm());
 //        scene.getStylesheets().add(getClass().getResource("calculator2.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
